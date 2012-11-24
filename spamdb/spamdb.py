885957e486cd76b603de3088f4563b98fad0a4e4
Hernan Lozano <hernantz@gmail.com>
2012-11-24 16:14:10 -0300

    Added spamdb class methods

--- spamdb/spamdb.py
+++ spamdb/spamdb.py
@@ -1,1 +1,15 @@
+import peewee
 
+
+class Spamdb(list):
+
+    # shortcut to append
+    add = list.append
+
+    def __init__(self, *args):
+        for a in args:
+            self.append(a)
+
+    def run(self):
+        for model in self.__iter__():
+            print model
