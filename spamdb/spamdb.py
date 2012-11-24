e00d9c9e1d72bf06ca6b22dceba2893fb64cb456
Hernan Lozano <hernantz@gmail.com>
2012-11-24 19:49:30 -0300

    Added decorators and run method to Spamdb class

--- spamdb/spamdb.py
+++ spamdb/spamdb.py
@@ -1,4 +1,5 @@
 import peewee
+import inspect
 
 
 class Spamdb(list):
@@ -7,9 +8,63 @@ class Spamdb(list):
     add = list.append
 
     def __init__(self, *args):
+        # allow passing models as positional arguments
+        # so it is possible to do s = Spamdb(model, another_model)
         for a in args:
             self.append(a)
 
+        # used to register custom handler for fields and types
+        self.global_handlers = {}
+        self.strict_handlers = {}
+
+    def _decorate(self, key, attr):
+        def fn(f):
+            attr.update({key: f})
+            return f
+        return fn
+
+    def strict_handler(self, field_qname):
+        """
+        Used to override default behaviour for a custom field in a model
+                Example usage:
+                import spamdb
+                import myapp.models
+
+
+                sdb = spamdb.Spamdb()
+
+                @sdb.strict_handler('models.User.name'):
+                def spam_username(field):
+                        # called when spamming User.name field
+                    return "Hi there!!"
+        """
+        return self._decorate(field_qname, self.strict_handlers)
+
+    def global_handler(self, field_type):
+        """
+        Used to override default behaviour for a field type in all models
+                Example usage:
+                import spamdb
+                import myapp.models
+
+
+                sdb = spamdb.Spamdb()
+
+                @sdb.global_handler('CharField'):
+                def spam_charfield(field):
+                        # called when spamming all peewee.CharField fields
+                    return "My custom string"
+        """
+        return self._decorate(field_type, self.global_handlers)
+
     def run(self):
+        """
+        Iterates throw all models and their peewee attrs to
+        spam them accordingly
+        """
         for model in self.__iter__():
-            print model
+            for k, v in inspect.getmembers(model):
+                if type(v) == peewee.CharField:
+                    print k, type(v)
+                elif type(v) == peewee.DateTimeField:
+                    print k, type(v)
